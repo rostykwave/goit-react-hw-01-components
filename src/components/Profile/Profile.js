2f6c57c18171650f899950fa134ffc89e9cffe04
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-30 20:31:45 +0300

    1

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+
+ const Profile = ({ avatar, username, tag, location, stats }) => {
+    // const { avatar, username, tag, location, stats } = props;
+    const { followers, views, likes } = stats;
+
+    return (
+<div className="profile">
+  <div className="description">
+    <img
+      src={avatar}
+      alt={`${username} avatar`}
+      className="avatar"
+    />
+    <p className="name">{username}</p>
+    <p className="tag">{tag}</p>
+    <p className="location">{location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{likes}</span>
+    </li>
+  </ul>
+</div>
+    )
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+}
+
+export default Profile;
