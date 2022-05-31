261df5ba9d750f56bb351ad14b646145b8347ca4
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-31 17:10:15 +0300

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,18 +12,18 @@ export const App = () => {
   return (
     <div>
 
-      {/* <Profile
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      /> */}
+      />
 
-      {/* <Statistics
+      <Statistics
         title="Upload stats"
         stats={data}
-      /> */}
+      />
 
       <FriendList
         friends={friends}
