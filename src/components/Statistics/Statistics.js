704ba37b53ee99292c20e532d9af71557cfc31de
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-31 16:05:05 +0300

    Statsitics + random color

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+import getRandomHexColor from '../Functions/getRandomHexColor';
 
 const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      <h2 className="title">{title}</h2>
-      <ul className="stat-list">
+    <section className={s.statistics}>
+      <h2 className={s.title}>{title}</h2>
+      <ul className={s.statList}>
         {stats.map(({ id, label, percentage }) => (
-          <li className="item" key={id}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}%</span>
+          <li
+            className={s.item}
+            key={id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
